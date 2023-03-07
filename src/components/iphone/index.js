352ace0c3186ce3b99a4a93aa75c9d3d6bd900e8
Raiyan Chowdhury <95684891+raiyan-chowdhury@users.noreply.github.com>
2023-03-07 14:12:32 +0000

    Update index.js

--- src/components/iphone/index.js
+++ src/components/iphone/index.js
@@ -30,7 +30,7 @@ export default class Iphone extends Component {
 			success : this.parseResponse,
 			error : function(req, err){ console.log('API call failed ' + err); }
 		})
-		// once the data grabbed, hide the button
+		// once the data is grabbed, hide the button
 		this.setState({ display: false });
 	}
 
