33aee589e2c124502d5a1cfc1da91fd680795a29
maher2603 <maher2603@hotmail.co.uk>
2023-03-15 11:39:49 +0000

    location page buttons

--- src/components/iphone/Location.js
+++ src/components/iphone/Location.js
@@ -12,7 +12,15 @@ export default class Location extends Component {
 
 	render() {
 		return (
-			<h1>Location</h1>
+			<div class={ style.container }>
+				<div><h1>Location</h1></div>
+				<div class={ style.header }>
+					<div class={ style.navigation }>
+						<div> { this.state.data_grabbed ? <a href = "./components/iphone/index.js"> <button>Home</button> </a>: null } </div>
+						<div> { this.state.data_grabbed ? <a href = "./components/iphone/location.js"> <button>Location</button> </a>: null } </div>
+					</div>
+					
+			</div>
 		);
 	}
 }
