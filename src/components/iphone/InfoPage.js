b17a93d4383ba6f6d59ac30dfbdf2f09cdc8db28
MustaqeemChoudhury <79055156+MustaqeemChoudhury@users.noreply.github.com>
2023-03-23 13:51:33 +0000

    Stable (unfinished) release
    Should run with npm run dev, some styling has been lost

--- src/components/iphone/InfoPage.js
+++ src/components/iphone/InfoPage.js
@@ -1,3 +1,4 @@
+
 import { h, Component } from 'preact';
 import FindLocationWeather from './FindLocationWeather.js';
 import style from './style';
@@ -24,7 +25,7 @@ class InfoPage extends Component {
 	}
 
 	// decide what image to show depending on the main weather
-  	mainWeatherImage = (main_weather) => {
+    mainWeatherImage = (main_weather) => {
 		if (main_weather == "Clear") {
 		return <img id="clear-icon" src="./assets/icons/sunny_bg.svg" alt="Clear Icon"></img>
 		} else if (main_weather == "Clouds") {
@@ -41,22 +42,23 @@ class InfoPage extends Component {
 		return <img id="atmosphere-icon" src="./assets/icons/misty_bg.svg" alt="Atmosphere Icon"></img>
 		}
   	}
-  	// decide what image to show depending on the weather in the 6h,12h,24h forecast section
+  	
+	// decide what image to show depending on the weather in the 6h,12h,24h forecast section
 	miniWeatherImage = (main_weather) => {
 		if (main_weather == "Clear") {
-			return <img id="clear-icon" src="./assets/icons/sunny_svg.svg" alt="Clear Icon"></img>
+			return <img id="clear-icon" style="filter: invert(87.5%);" src="./assets/icons/sunny_svg.svg" alt="Clear Icon"></img>
 		} else if (main_weather == "Clouds") {
-			return <img id="cloudy-icon" src="./assets/icons/cloudy_svg.svg" alt="Cloudy Icon"></img>
+			return <img id="cloudy-icon" style="filter: invert(87.5%);" src="./assets/icons/cloudy_svg.svg" alt="Cloudy Icon"></img>
 		} else if (main_weather == "Rain") {
-			return <img id="rainy-icon" src="./assets/icons/rainy_svg.svg" alt="Rainy Icon"></img>
+			return <img id="rainy-icon" style="filter: invert(87.5%);" src="./assets/icons/rainy_svg.svg" alt="Rainy Icon"></img>
 		} else if (main_weather == "Thunderstorm") {
-			return <img id="thunderstorm-icon" src="./assets/icons/thunderstorm_svg.svg" alt="Thunderstorm Icon"></img>
+			return <img id="thunderstorm-icon" style="filter: invert(87.5%);" src="./assets/icons/thunderstorm_svg.svg" alt="Thunderstorm Icon"></img>
 		} else if (main_weather == "Drizzle") {
-			return <img id="drizzle-icon" src="./assets/icons/drizzle_svg.svg" alt="Drizzle Icon"></img>
+			return <img id="drizzle-icon" style="filter: invert(87.5%);" src="./assets/icons/drizzle_svg.svg" alt="Drizzle Icon"></img>
 		} else if (main_weather == "Snow") {
-			return <img id="snow-icon" src="./assets/icons/snow_svg.svg" alt="Snow Icon"></img>
+			return <img id="snow-icon" style="filter: invert(87.5%);" src="./assets/icons/snow_svg.svg" alt="Snow Icon"></img>
 		} else if (main_weather == "Mist" || main_weather == "Smoke" || main_weather == "Haze" || main_weather == "Fog") {
-			return <img id="atmosphere-icon" src="./assets/icons/foggy_svg.svg" alt="Atmosphere Icon"></img>
+			return <img id="atmosphere-icon" style="filter: invert(87.5%);" src="./assets/icons/foggy_svg.svg" alt="Atmosphere Icon"></img>
 		}
 	}
     
@@ -101,7 +103,6 @@ class InfoPage extends Component {
     if (this.state.data_grabbed == false) {
         return (
         <div>
-            <h1>infoPage Hello</h1>
             <FindLocationWeather onLocationFetch={this.showPosition} />
         </div>
         )
@@ -113,79 +114,69 @@ class InfoPage extends Component {
         const contaStyles = this.state.main ? `${style.conta}` : style.conta2;
         const conta3Styles = this.state.main ? `${style.conta3}` : style.conta2;
         return (
-        <div class={ style.container }>
-            <div class={ style.header }>
-                <div class={contaStyles}>
-                    <div class={ style.city }>
-                        { this.state.locate }
-                    </div>
-                    <div class={conta3Styles}>
-                        <div class={style.weather_img}>
-                            { this.state.data_grabbed ? this.mainWeatherImage(this.state.main): null }
-                        </div>		
-                        <div class={ style.conditions } >
-                            { this.state.main }
+            <div class={ style.container2 }>
+                <div class={ style.header }>
+                    <div class={contaStyles}>
+                        <div class={ style.city }>
+                            { this.state.locate }
                         </div>
-                        <span class={ tempStyles }>
-                            { this.state.temp }
-                        </span>
+                        <div class={conta3Styles}>
+                            <div class={style.weather_img}>
+                                { this.state.data_grabbed ? this.mainWeatherImage(this.state.main): null }
+                            </div>		
+                            <div class={ style.conditions } >
+                                { this.state.main }
+                            </div>
+                            <span class={ tempStyles }>
+                                { this.state.temp }
+                            </span>
+                        </div>
+                        <div>
+                            {this.state.data_grabbed ? <h3 class={style.weatherLabel}>Precipitation</h3> : null}
+                        </div>
+                        <div class={ style.precipitation }>
+                            { this.state.data_grabbed ? <img id="rainy-icon" style="padding:0px;padding-right: 160px; vertical-align: bottom; filter: invert(87.5%)" src="./assets/icons/pop_svg.svg" alt="Raining Icon"></img> : null }
+                            { this.state.prec }
+                        </div>
+                        <div id="windspeed">
+                            {this.state.data_grabbed ? <h3 class={style.weatherLabel}>Wind Speed</h3> : null}
+                        </div>
+                        <div class={ style.windspeed }>
+                            { this.state.data_grabbed ? <img id="windspeed-icon"  src="./assets/icons/windspeed_svg.svg" alt="Wind Speed Icon"style="padding:0; margin:0;padding-right: 130px; vertical-align: bottom; filter: invert(87.5%)"></img> : null }
+                            { this.state.wspeed }
+                        </div>
                     </div>
-                    <div>
-                        {this.state.data_grabbed ? <h3>Precipitation</h3> : null}
+                    <h3 class={style.forecast} style="margin-left:70px; padding-bottom:-3% margin-top:1%; font-size:larger;">
+                        Forecast
+                    </h3>
+                    <div> { this.state.data_grabbed ? <button class={style.Weeklybutton} onClick={() => this.setState({ location:"", data_grabbed: false })}>Reset</button> : null }</div>
+                    <div class={ style.forecast_text }>
+                        <p class={ style.forecast_text }>6h</p>
+                        <p class={ style.forecast_text }>12h</p>
+                        <p class={ style.forecast_text }>24h</p>
                     </div>
-                    <div class={ style.precipitation }>
-                        { this.state.data_grabbed ? <img id="rainy-icon" style="padding:0px;padding-right: 160px; vertical-align: bottom;" src="./assets/icons/pop_svg.svg" alt="Raining Icon"></img> : null }
-                        { this.state.prec }
-                    </div>
-                    <div id="windspeed">
-                        {this.state.data_grabbed ? <h3>Wind Speed</h3> : null}
-                    </div>
-                    <div class={ style.windspeed }>
-                        { this.state.data_grabbed ? <img id="windspeed-icon"  src="./assets/icons/windspeed_svg.svg" alt="Wind Speed Icon"style="padding:0; margin:0;padding-right: 125px; vertical-align: bottom;"></img> : null }
-                        { this.state.wspeed }
+                    <div class={ style.forecast }>	
+                        <div class={ style.forecast_image }>
+                            { this.state.data_grabbed ? this.miniWeatherImage(this.state.main_6h): null }
+                            <p class={ style.forecast_textLabel }>{this.state.main_6h} </p>
+                        </div>
+                        <div class={style.vl}></div>
+                        <div class={ style.forecast_image }>
+                            { this.state.data_grabbed ? this.miniWeatherImage(this.state.main_12h): null }
+                            <p class={ style.forecast_textLabel }>{this.state.main_12h}</p>
+                        </div>
+                        <div class={style.vl}></div>
+                        <div class={ style.forecast_image }>
+                            { this.state.data_grabbed ? this.miniWeatherImage(this.state.main_24h): null }
+                            <p class={ style.forecast_textLabel }>{this.state.main_24h}</p>
+                        </div>
                     </div>
                 </div>
-                <h3 class={style.forecast} style="margin-left:70px; padding-bottom:-3% margin-top:1%; font-size:larger;">
-                    Forecast
-                </h3>
-                <div class={ style.forecast_text }>
-                    <p class={ style.forecast_text }>6h</p>
-                    <p class={ style.forecast_text }>12h</p>
-                    <p class={ style.forecast_text }>24h</p>
-                </div>
-                <div class={ style.forecast }>	
-                    <div class={ style.forecast_image }>
-                        { this.state.data_grabbed ? this.miniWeatherImage(this.state.main_6h): null }
-                        <p class={ style.forecast_textLabel }>{this.state.main_6h} </p>
-                    </div>
-                    <div class={style.vl}></div>
-                    <div class={ style.forecast_image }>
-                        { this.state.data_grabbed ? this.miniWeatherImage(this.state.main_12h): null }
-                        <p class={ style.forecast_textLabel }>{this.state.main_12h}</p>
-                    </div>
-                    <div class={style.vl}></div>
-                    <div class={ style.forecast_image }>
-                        { this.state.data_grabbed ? this.miniWeatherImage(this.state.main_24h): null }
-                        <p class={ style.forecast_textLabel }>{this.state.main_24h}</p>
-                    </div>
-                    <div> { this.state.data_grabbed ? <button class={style.Weeklybutton} onClick={() => this.setState({ location:"", data_grabbed: false})}>Reset</button> : null }</div>
-
-                </div>
+                <div class={ style.details }></div>
             </div>
-            <div class={ style.details }></div>
-        </div>
-    )};
+        );
+    }
     }
 }
 
 export default InfoPage;
-
-/*
-NOTES:
-- This file will do data handling for the location submitted by the file
-    - Includes Geocaching location into lang/long
-- In the main render part (in the appropriate part of giant If) the same processes for main page will be adapted
-    - Might need to remake the main file code into a new file (think about props)
-
-    <div> { this.state.data_grabbed ? <button class={style.Weeklybutton} onClick={() => this.setState({ on_home_page:false, on_weekly_weather_page: true })}>Week</button> : null }</div>
-*/
